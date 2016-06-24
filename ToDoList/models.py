63c3ef6b99a6b607cf145333c23ed36b7d6ded90
Daniel Betteridge <danielrbetteridge@gmail.com>
2016-06-24 21:57:10 +0800

    add item model

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -3,10 +3,13 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Item(models.Model):
+	taskItem = models.CharField(max_length=255, default="")
+
 class Task(models.Model):
     taskName = models.CharField(max_length=30)
-    taskItems = models.CharField(max_length=255, default="")
+    taskItems = models.ForeignKey(Item, null=True)
     taskCompleted = models.BooleanField()
     taskImportance = models.IntegerField(default=1, validators=[MaxValueValidator(3), MinValueValidator(1)])
     creator = models.ForeignKey(User, null=True)
-
