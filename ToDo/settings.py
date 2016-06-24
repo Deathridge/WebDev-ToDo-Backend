e17b42554ea3a2f3a0e94e0c7c81f08b7733be10
Deathridge <danielrbetteridge@gmail.com>
2016-06-24 14:34:17 +0800

    update to token/basic auth

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -83,7 +83,8 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     # Use standard 'django.contrib.auth' permissions
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
